d84124515ac154ce057b771947847a51c623d765
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2021-08-09 21:25:27 -0300

    Create Produto.java

---
+++ ListaValendoNota/questao14/Produto.java
@@ -0,0 +1,16 @@
+public class Produto extends Lista {
+    String nomeProduto;
+    double valor;
+    int quantidade;
+
+    public void preencheProd(String np, double vl, int qtd){
+        nomeProduto = np;
+        valor = vl;
+        quantidade = qtd;
+    }
+
+    public String toString(){
+        return "Produto: "+nomeProduto+" | Valor: "+valor+" | Qtd: "+quantidade;
+    }
+}
+
