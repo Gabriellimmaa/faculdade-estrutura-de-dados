8d0c1a5ba8f24fd00afaeb04ac130dc8e72f6fde
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2021-09-08 16:05:35 -0300

    Update No.java

--- lista encadeada dinamica simples com NO/No.java
+++ lista encadeada dinamica simples com NO/No.java
@@ -1,1 +1,30 @@
+public class No {
+    Object obj;
+    No proximo;
 
+    String nome;
+    Double altura;
+    Double peso;
+    String esporte;
+    String patrocinadores;
+
+    public No(String _nome, Double _altura, Double _peso, String _esporte, String _patrocinadores){
+        nome = _nome;
+        altura = _altura;
+        peso = _peso;
+        esporte = _esporte;
+        patrocinadores = _patrocinadores;
+    }
+
+    public String toString(){
+        return "Nome: "+nome+" Altura: "+altura+" Peso: "+peso+" Esporte: "+esporte+" Patrocinadores: "+patrocinadores;
+    }
+
+    public void setProximo(No n){
+        proximo = n;
+    }
+    public No getProximo(){
+        return proximo;
+    }
+
+}
