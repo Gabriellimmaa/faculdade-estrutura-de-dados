af4296ef347c73bea7b70d63d3f43e61994979c9
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2021-09-10 23:08:33 -0300

    Create Alunos.java

---
+++ Lista duplamente encadeada/Alunos.java
@@ -0,0 +1,18 @@
+public class Aluno {
+    String nome;
+    int matricula;
+    double nota;
+    String curso;
+
+    public Aluno(String a, int b, double c, String d){
+        nome = a;
+        matricula = b;
+        nota = c;
+        curso = d;
+
+    }
+
+    public String toString(){
+        return "Aluno: "+nome+"| Matricula: "+matricula+"| Nota: "+nota+"| Curso: "+curso;
+    }
+}
