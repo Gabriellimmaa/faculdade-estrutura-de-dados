5a341f5d04b004560be3a6e08191a33968352dc2
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2021-09-13 17:09:32 -0300

    Create No.java

---
+++ Implementando FILA e PILHA utilizando a estrutura NO de uma lista duplamente encadeada/No.java
@@ -0,0 +1,11 @@
+public class No {
+    int valor;
+
+    public No(int _valor) {
+        this.valor = _valor;
+    }
+
+    public String tostring() {
+        return "Valor: ["+valor+"]";
+    }
+}
