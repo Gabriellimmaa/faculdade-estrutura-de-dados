b14405525ce7a1ca86e7992763355e0383b6820e
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2021-09-14 20:01:50 -0300

    Update No.java

--- Implementando FILA e PILHA utilizando a estrutura NO de uma lista duplamente encadeada/No.java
+++ Implementando FILA e PILHA utilizando a estrutura NO de uma lista duplamente encadeada/No.java
@@ -1,11 +1,40 @@
 public class No {
     int valor;
+    No prox;
+    No ant;
 
-    public No(int _valor) {
-        this.valor = _valor;
+    public No(int x){
+        setValor(x);
     }
 
-    public String tostring() {
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public No getProx() {
+        return prox;
+    }
+
+    public void setProx(No prox) {
+        this.prox = prox;
+    }
+
+    public No getAnt() {
+        return ant;
+    }
+
+    public void setAnt(No ant) {
+        this.ant = ant;
+    }
+
+    @Override
+    public String toString(){
         return "Valor: ["+valor+"]";
+
     }
+
 }
